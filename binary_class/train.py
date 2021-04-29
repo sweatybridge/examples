ee925f778b6e2092bd12a269b55f6f8952a58dba
Han Qiao <qiao@basis-ai.com>
2021-04-29 17:00:33 +0800

    Try not negating

--- binary_class/train.py
+++ binary_class/train.py
@@ -88,8 +88,12 @@ def main():
     features = model_data[FEATURE_COLS].iloc[selected]
     inference = clf.predict_proba(features)[:, 1]
     val = inference.tolist()
+    print(np.histogram(val, dtype=float))
     print(np.histogram(-np.asarray(val, dtype=float)))
+    print("-")
     print(np.histogram_bin_edges(val, bins="auto"))
+    print(np.histogram_bin_edges(-np.asarray(val, dtype=float), bins="auto"))
+    print("-")
     print(get_bins(val))
     print(fast_histogram(val))
 
